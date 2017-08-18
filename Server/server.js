dff2fd1e229108ad9f054723e64cd31a0f5907da
Gene Coleman <gcoleman81@gmail.com>
2017-08-18 16:09:44 -0500

    typo corrections

--- Server/server.js
+++ Server/server.js
@@ -46,7 +46,7 @@ app.get('/todos/:id', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Started on port 3000 ${port}`);
+  console.log(`Started on port ${port}`);
 });
 
 module.exports = {app};
