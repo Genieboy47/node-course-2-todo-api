c6021a48fdd3c679a83db9f00cf2855a03fb72a1
Gene Coleman <gcoleman81@gmail.com>
2017-08-23 15:10:07 -0500

    added user login route

--- Server/server.js
+++ Server/server.js
@@ -103,6 +103,16 @@ var user = new User(body)
   });
 });
 
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    rest.status(400).send();
+  })
+});
 
 
 app.get('/users/me', authenticate, (req, res) => {
